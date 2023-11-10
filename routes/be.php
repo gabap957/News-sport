b4ad27dfab958f000f9a31a37abdc74821d31bf1
Trần Hữu Cường <139962350+gabap957@users.noreply.github.com>
2023-11-10 16:05:09 +0700

    update

--- routes/be.php
+++ routes/be.php
@@ -1,11 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\CategoryController;
 
 Route::prefix('/admin')->group(function (){
     Route::prefix('/user')->group(function (){
-        Route::get('/',[UserController::class, 'list'])->name('admin.user.list');
+        Route::get('/',[AdminController::class, 'list'])->name('admin.user.list');
+    });
+    Route::prefix('/category')->group(function (){
+        Route::get('/',[CategoryController::class, 'list'])->name('admin.category.list');
     });
 });
 ?>
