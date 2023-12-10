6a7cdc381b7213a8c0a8900eec70efaacdcbb227
Trần Hữu Cường <139962350+gabap957@users.noreply.github.com>
2023-12-10 13:50:25 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,14 +6,24 @@ use App\Models\category;
 use App\Models\image;
 use App\Models\post;
 use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\DB;
 
 
 class HomeController extends Controller
 {
     public function home(){
         $categoryParent = category::where('parent_id',0)->get();
-        $post = post::ORDERBY('id','DESC')->limit(5)->get();
-        return view('fe.home',compact('post','categoryParent'));
+        $post = DB::table('posts')->get()->groupBy('type_id');
+        foreach ($post as $key => $value) {
+            $post[$key] = $value;
+            if(count($post[$key])==1){
+                $tindacbiet=$post[$key];
+            }
+            if(count($post[$key])==4){
+                $tinNoibat=$post[$key];
+            }   
+        }
+        return view('fe.home',compact('post','categoryParent','tindacbiet','tinNoibat'));
     }
 
     public function search(Request $request){
