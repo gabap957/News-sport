056cd50a8f6902520bd9f23943df915d63d863e5
Trần Hữu Cường <139962350+gabap957@users.noreply.github.com>
2023-12-27 21:38:27 +0700

    update

--- resources/views/be/layout.blade.php
+++ resources/views/be/layout.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Blank</title>
+    <title>News Sports Admin</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{asset('/adminlte/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
